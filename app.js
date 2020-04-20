8c696aee5e067d9410f1c479cc290e11dc6f0b3c
Timits <will4f21@yahoo.com>
2020-04-20 04:23:07 -0700

    created form email/passwrd

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, 'static','index.html'));
+});
+
+app.post('/', (req, res) => {
+  //const obj = JSON.parse(JSON.stringify(req.body));
+  console.log(req.body);
+  // data base work here 
+  res.send('successfully posted data');
+});
+
+
+app.listen('3005');
---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+//console.log('Testing client side');
+
+
+
+
+
+
